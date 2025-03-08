9f4ee71d9c98b7ed416a99097143ee1b6166261d
Kudae Sithu <kudaesithu2@gmail.com>
2025-03-08 20:32:23 +0630

    Implemented models, and global error middleware

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';
 import subRouter from './routes/subscription.routes.js';
 import connectToDB from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Routes
 app.get('/', (req, res) => {
     res.send('Welcome to the Subscription tracker API');
 })
@@ -15,6 +23,10 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subRouter)
 
+// Error handling middleware (global error handler)
+app.use(errorMiddleware);
+
+// Start the server
 app.listen(PORT, async() => {
     await connectToDB();
     console.log(`Subscription tracking is listening on http://localhost:${PORT}/`);
