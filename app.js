b8b3ad9c4f055eed524d4283c0363ca778185d84
Kudae Sithu <kudaesithu2@gmail.com>
2025-03-18 20:36:31 +0630

    fixed arcjet

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import subRouter from './routes/subscription.routes.js';
 import connectToDB from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 import cookieParser from 'cookie-parser';
-// import arcjetMiddleware from './middlewares/arcjet.middleware.js';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 
 const app = express();
 
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(arcjetMiddleware);
+app.use(arcjetMiddleware);
 
 // Routes
 app.get('/', (req, res) => {
